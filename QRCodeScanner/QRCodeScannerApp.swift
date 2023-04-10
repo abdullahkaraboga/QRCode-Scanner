93ffc3d47a049ab37da18f519cc7abec33de5f70
abdullahkaraboga <abdullahkaraboga@icloud.com>
2023-04-10 22:30:18 +0300

    Initial Commit

---
+++ QRCodeScanner/QRCodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeScannerApp.swift
+//  QRCodeScanner
+//
+//  Created by Abdullah Karaboğa on 10.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
